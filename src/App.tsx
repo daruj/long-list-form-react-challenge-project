c90e4d070d7623347cbd3cbd04c23f859a9a0bee
Damian Aruj <aruj.damian@gmail.com>
2023-09-29 19:34:59 +0300

    Added React Query Provider to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,24 @@ import NavBar from './components/NavBar';
 import StatisticsPage from './pages/statistics/StatisticsPage';
 import UsersPage from './pages/users/UsersPage';
 import { ContextProvider } from './context/usersContext';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <ContextProvider>
-        <Routes>
-          <Route path="/" caseSensitive element={<StatisticsPage />} />
-          <Route path="users" element={<UsersPage />} />
-        </Routes>
-      </ContextProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <NavBar />
+        <ContextProvider>
+          <Routes>
+            <Route path="/" caseSensitive element={<StatisticsPage />} />
+            <Route path="users" element={<UsersPage />} />
+          </Routes>
+        </ContextProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
