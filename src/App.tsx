ae03542bd866ad5d0d90fc91ad834e9157ff0b8c
Damian Aruj <aruj.damian@gmail.com>
2023-09-29 20:09:15 +0300

    Removed usersContext since I will not use it anymore

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import StatisticsPage from './pages/statistics/StatisticsPage';
 import UsersPage from './pages/users/UsersPage';
-import { ContextProvider } from './context/usersContext';
 import { QueryClientProvider, QueryClient } from 'react-query';
 
 // Create a client
@@ -13,12 +12,10 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <NavBar />
-        <ContextProvider>
-          <Routes>
-            <Route path="/" caseSensitive element={<StatisticsPage />} />
-            <Route path="users" element={<UsersPage />} />
-          </Routes>
-        </ContextProvider>
+        <Routes>
+          <Route path="/" caseSensitive element={<StatisticsPage />} />
+          <Route path="users" element={<UsersPage />} />
+        </Routes>
       </BrowserRouter>
     </QueryClientProvider>
   );
