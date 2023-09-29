1a1a79e1b7aaf13dba57e5985ea86b837f5d774a
Damian Aruj <aruj.damian@gmail.com>
2023-09-29 14:51:26 +0300

    installed typescript and updated rules in eslint to support ts and tsx files, also added some new rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,34 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
+    // Make sure it's always the last config, so it gets the chance to override other configs.
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use.
+      version: 'detect',
+    },
+    // Tells eslint how to resolve imports
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 0,
     'react/prop-types': 0,
+    'no-console': ['warn', { allow: ['clear', 'info', 'error', 'dir', 'trace'] }],
+    'react/jsx-uses-react': 'off', // Turn off the original rule
+    'react/react-in-jsx-scope': 'off', // Enable this rule to enforce React in scope
   },
 };
