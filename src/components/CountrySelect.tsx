2a2d9497761d2afaa57e32858e187340e4c455d5
Damian Aruj <aruj.damian@gmail.com>
2023-10-01 03:25:55 +0300

    Created AutoComplete for CountrySelect

--- src/components/CountrySelect.tsx
+++ src/components/CountrySelect.tsx
@@ -1,6 +1,5 @@
-import Autocomplete from '@mui/material/Autocomplete';
+import { Autocomplete, TextField } from '@mui/material';
 import countryOptions from '@src/data/countries.json';
-import { TextField } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const StyledAutocomplete = styled(Autocomplete)({
@@ -8,21 +7,38 @@ const StyledAutocomplete = styled(Autocomplete)({
   textTransform: 'none',
   backgroundColor: '#909196',
   borderRadius: '4px',
-  '& input': {
-    padding: '0 14px !important',
-  },
 });
 
 interface CountrySelectProps {
   value: string;
+  error: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onChange: (...event: any) => void;
+  onKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => void;
 }
 
-const CountrySelect: React.FC<CountrySelectProps> = ({ value }) => {
+const CountrySelect: React.FC<CountrySelectProps> = ({
+  value,
+  error,
+  onChange,
+  onKeyDown,
+}) => {
   return (
     <StyledAutocomplete
       options={countryOptions}
-      disableCloseOnSelect
-      renderInput={(params) => <TextField {...params} />}
+      renderInput={(params) => {
+        return (
+          <TextField
+            {...params}
+            variant="outlined"
+            onChange={onChange}
+            error={error}
+            placeholder="Select Country"
+            onKeyDown={onKeyDown}
+          />
+        );
+      }}
+      onChange={(_, values) => onChange(values)}
       value={value}
     />
   );
